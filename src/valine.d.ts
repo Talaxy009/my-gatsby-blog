da8930b241d2b576d6e0faba89244f42570f7b0e
Talaxy009 <x009ba1@gmail.com>
2022-11-27 15:17:51 +0800

    🔥 删除不再使用的代码

--- src/valine.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'valine' {
-	class Valine {
-		constructor(options?: ValineOptions);
-		init(options: ValineOptions);
-	}
-	export = Valine;
-}
--- valine-config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	appId: 'uNA1YWiX0B1smHYzKFAUD9kS-9Nh9j0Va',
-	appKey: 'uTK9cTmOLI8pQ7WSGGmKMP2K',
-	placeholder: '你知道吗？Home 键可以把光标放到行首诶！',
-	avatar: 'mp',
-	pageSize: 5,
-};
