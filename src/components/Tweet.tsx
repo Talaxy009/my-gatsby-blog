dc6fb7f903744bf09de80f3b539fc8041762570b
Talaxy009 <x009ba1@gmail.com>
2023-05-07 22:06:23 +0800

    🐛 修复 React SSR 问题

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import {Tweet as ReactTweet} from 'react-tweet';
-import { useSiteMetadata } from '../utils/hooks';
+import {Tweet as ReactTweet, TweetSkeleton} from 'react-tweet';
+import {useHasMounted, useSiteMetadata} from '../utils/hooks';
 
 export default function Tweet({id = ''}) {
-    const siteMetaData = useSiteMetadata();
+	const siteMetaData = useSiteMetadata();
+	const hasMounted = useHasMounted();
 
-    return <ReactTweet apiUrl={`${siteMetaData.siteUrl}/api/tweet/${id}`} />
+	return hasMounted ? (
+		<ReactTweet apiUrl={`${siteMetaData.siteUrl}/api/tweet/${id}`} />
+	) : (
+		<TweetSkeleton />
+	);
 }
