b022e9bb2b90c2a12df8af43515f333a8ef3753c
Talaxy009 <x009ba1@gmail.com>
2023-05-16 21:32:57 +0800

    💫 使用 ViewTransition API 添加夜间模式动画

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -12,13 +12,45 @@ export default function DarkModeButton() {
 	const hasMounted = useHasMounted();
 	const [mode, {toggle}] = useDarkMode();
 
+	const handleClick = (event: React.MouseEvent) => {
+		// @ts-expect-error: Transition API
+		if (!document.startViewTransition) {
+			toggle();
+			return;
+		}
+		const x = event.clientX;
+		const y = event.clientY;
+		const endRadius = Math.hypot(
+			Math.max(x, innerWidth - x),
+			Math.max(y, innerHeight - y),
+		);
+		// @ts-expect-error: Transition API
+		const transition = document.startViewTransition(toggle);
+		transition.ready.then(() => {
+			const clipPath = [
+				`circle(0px at ${x}px ${y}px)`,
+				`circle(${endRadius}px at ${x}px ${y}px)`,
+			];
+			document.documentElement.animate(
+				{
+					clipPath,
+				},
+				{
+					duration: 500,
+					easing: 'ease-in',
+					pseudoElement: '::view-transition-new(root)',
+				},
+			);
+		});
+	};
+
 	if (!hasMounted)
 		return <Skeleton variant="circular" width={51} height={51} />;
 	return (
 		<Tooltip title={`${mode ? '关闭' : '打开'}夜间模式`}>
-			<IconButton className='darkModeBtn' onClick={toggle}>
-				<MoonIcon id="moon-icon" htmlColor='#ffb74d' fontSize='large' />
-				<SunIcon id="sun-icon" htmlColor='#ff9800' fontSize='large' />
+			<IconButton className="darkModeBtn" onClick={handleClick}>
+				<MoonIcon id="moon-icon" htmlColor="#ffb74d" fontSize="large" />
+				<SunIcon id="sun-icon" htmlColor="#ff9800" fontSize="large" />
 			</IconButton>
 		</Tooltip>
 	);
