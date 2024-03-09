c8f14118dae800c690c8067dd37fc0d662806d2e
Talaxy009 <x009ba1@gmail.com>
2024-03-09 15:18:51 +0800

    更新评论 function 代码

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -1,13 +1,11 @@
-process.env.DETA_RUNTIME = 'true';
-
 const http = require('http');
 const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
 
 const app = Waline({
+	env: 'netlify',
 	async postSave() {
 		// do what ever you want after save comment
-
 	},
 });
 
